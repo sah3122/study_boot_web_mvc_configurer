9a9b7e8b25982296b13604e4a2fcd124f9987051
dongchul <sah3122@naver.com>
2019-10-28 13:05:08 +0900

    ADD Handler Interceptor part 2

---
+++ src/main/java/me/study/study_boot_web_mvc_confiturer/AnotherInterceptor.java
@@ -0,0 +1,29 @@
+package me.study.study_boot_web_mvc_confiturer;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Created by dongchul on 2019-10-28.
+ */
+public class AnotherInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        System.out.println("preHandle 2");
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        System.out.println("postHandle 2");
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        System.out.println("afterCompletion 2");
+    }
+}
--- src/main/java/me/study/study_boot_web_mvc_confiturer/WebConfig.java
+++ src/main/java/me/study/study_boot_web_mvc_confiturer/WebConfig.java
@@ -2,6 +2,7 @@ package me.study.study_boot_web_mvc_confiturer;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -11,4 +12,12 @@ public class WebConfig implements WebMvcConfigurer {
 //    public void addFormatters(FormatterRegistry registry) {
 //        registry.addFormatter(new PersonFormatter());
 //    }
+
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new GreettingInterceptor()).order(0);
+        registry.addInterceptor(new AnotherInterceptor())
+                .addPathPatterns("/hi/**").order(-1);
+    }
 }
